310e4ac9e8995cf9740fb8876adfe462d306c92c
unknown <vaibhav722322@gmail.com>
2025-04-26 17:31:45 +0530

    Add 2

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,4 +1,10 @@
 import dynamic from "next/dynamic";
+import First from "@/components/First";
+import Second from "@/components/Second";
+import Third from "@/components/Third";
+import Fourth from "@/components/Fourth";
+import Sixth from "@/components/Sixth";
+import Seventh from "@/components/Seventh";
 
 const First = dynamic(() => import("@/components/First"));
 const Second = dynamic(() => import("@/components/Second"));
@@ -9,16 +15,6 @@ const Seventh = dynamic(() => import("@/components/Seventh"));
 
 
 
-
-import First from "@/components/First";
-import Fourth from "@/components/Fourth";
-
-import Second from "@/components/Second";
-import Seventh from "@/components/Seventh";
-
-
-import Sixth from "@/components/Sixth";
-import Third from "@/components/Third";
 
 
 
